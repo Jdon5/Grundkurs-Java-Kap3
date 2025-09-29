2e78d71d5da86152692be22d66616924151e6fa7
Jdon5 <162381746+Jdon5@users.noreply.github.com>
2025-09-29 15:13:07 +0200

    clear Test Klasse

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,5 @@
 public class Test {
     public static void main(String[] args) {
 
-        int a = -1;
-        int b = 7;
-
-        System.out.println(a++ +b);
-        System.out.println();
     }
 }
